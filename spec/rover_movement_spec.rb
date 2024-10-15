5fbbccb0ab697db9fd4df353793459d8eab280d4
hov8aa <hov8aa@gmail.com>
2024-10-15 20:07:17 +0530

    Added another spec to complete the scope of previously added context but there is no code to execute this spec

--- spec/rover_movement_spec.rb
+++ spec/rover_movement_spec.rb
@@ -8,9 +8,13 @@ RSpec.describe "Let's learn to move the rover on mars, by first writing specs" d
         end
     end
 
-    context "validate if the rover moves left in all the directions as expected" do
+    context "validate if the rover moves in all the directions as expected" do
         it "should check if the rover face direction before left turn and after left turn are as expected" do
             expect(turn_rover_left('E')).to eq('N')
+        end
+
+        it "should check if the rover face direction before right turn and after right turn are as expected" do
+            expect(turn_rover_right('E')).to eq('S')
         end
     end
 end
