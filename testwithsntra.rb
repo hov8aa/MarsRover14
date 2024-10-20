506b9bd9de4e0cac2653dcec84a8fd559e5b047f
hov8aa <hov8aa@gmail.com>
2024-10-20 11:15:33 +0530

    added sinatra code to test all the ruby codes & successfully tested all the functions using postman

--- testwithsntra.rb
+++ testwithsntra.rb
@@ -31,3 +31,44 @@ get '/move_rover_forward' do
     "The result is #{result}"
 end
 
+#Test this function using http://localhost:4567/move_rover_forward_in_any_direction?rover_x_coordinate=10&rover_y_coordinate=33&direction=N
+get '/move_rover_forward_in_any_direction' do
+    rover_x_coordinate = params['rover_x_coordinate'].to_i
+    rover_y_coordinate = params['rover_y_coordinate'].to_i
+    direction = params['direction'].upcase
+    result = move_rover_forward_in_any_direction(rover_x_coordinate,rover_y_coordinate,direction)
+    "The result is #{result}"
+end
+
+#http://localhost:4567/move_rover_forward_anywhere?rover_x_coordinate=10&rover_y_coordinate=33&direction=N&instructions=LMLMLMLMM
+get '/move_rover_forward_anywhere' do
+    rover_x_coordinate = params['rover_x_coordinate'].to_i
+    rover_y_coordinate = params['rover_y_coordinate'].to_i
+    direction = params['direction'].upcase
+    instructions = params['instructions'].upcase
+    result = move_rover_forward_anywhere(rover_x_coordinate,rover_y_coordinate,direction,instructions)
+    "The result is #{result}"
+end
+
+#http://localhost:4567/move_rover_forward_anywhere_with_direction_knowledge?rover_x_coordinate=10&rover_y_coordinate=33&direction=N&instructions=LMLMLMLMM
+#http://localhost:4567/move_rover_forward_anywhere_with_direction_knowledge?rover_x_coordinate=1&rover_y_coordinate=2&direction=N&instructions=LMLMLMLMM
+get '/move_rover_forward_anywhere_with_direction_knowledge' do
+    rover_x_coordinate = params['rover_x_coordinate'].to_i
+    rover_y_coordinate = params['rover_y_coordinate'].to_i
+    direction = params['direction'].upcase
+    instructions = params['instructions'].upcase
+    result = move_rover_forward_anywhere_with_direction_knowledge(rover_x_coordinate,rover_y_coordinate,direction,instructions)
+    "The result is #{result}"
+end
+
+#http://localhost:4567/move_rover_forward_anywhere_with_direction_knowledge?plateau_x_coordinate=5&plateau_y_coordinate=5&rover_x_coordinate=1&rover_y_coordinate=2&direction=N&instructions=LMLMLMLMM
+get '/move_rover_forward_within_plateau_boundaries' do
+    plateau_x_coordinate = params['plateau_x_coordinate'].to_i
+    plateau_y_coordinate = params['plateau_y_coordinate'].to_i
+    rover_x_coordinate = params['rover_x_coordinate'].to_i
+    rover_y_coordinate = params['rover_y_coordinate'].to_i
+    direction = params['direction'].upcase
+    instructions = params['instructions'].upcase
+    result = move_rover_forward_within_plateau_boundaries(plateau_x_coordinate,plateau_y_coordinate,rover_x_coordinate,rover_y_coordinate,direction,instructions)
+    "The result is #{result}"
+end
